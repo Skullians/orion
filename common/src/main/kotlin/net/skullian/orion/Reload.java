c7401063a54e58d5d84122f705f95572bbc47cec
Skullian <133133667+Skullians@users.noreply.github.com>
2026-04-26 20:50:59 +0100

    fix: oops

--- common/src/main/kotlin/net/skullian/orion/Reload.java
+++
@@ -1,9 +0,0 @@
-package net.skullian.orion;
-
-/**
- * This is an annotation.
- *
- * @author Skullians
- * @since 26/04/2026
- */
-public @interface Reload {}
---
+++ common/src/main/kotlin/net/skullian/orion/Reload.kt
@@ -0,0 +1,9 @@
+package net.skullian.orion
+
+/**
+ * This is a class.
+ *
+ * @author Skullians
+ * @since 26/04/2026
+ */
+annotation class Reload
